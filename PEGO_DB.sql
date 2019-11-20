00a0302e1efd0d67811dfc2dd2125a59b13e630e
Manoj Tiwari <manoj.tiwari@ica.se>
2019-11-20 11:37:00 +0530

    Polishing for presentation

--- PEGO_DB.sql
+++ PEGO_DB.sql
@@ -1,19 +1,4 @@
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: olx
--- ------------------------------------------------------
--- Server version	5.7.20-log
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE=`+00:00` */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE=`NO_AUTO_VALUE_ON_ZERO` */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- Database Setup Script
 
 --
 -- Table structure for table `category`
@@ -23,32 +8,22 @@ create database pego;
 use pego;
 
 DROP TABLE IF EXISTS `category`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `category` (
   `category_id` varchar(20) NOT NULL,
   `name` varchar(45) NOT NULL,
   `description` varchar(450) NOT NULL,
   PRIMARY KEY (`category_id`)
 );
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `category`
---
 
 LOCK TABLES `category` WRITE;
-/*!40000 ALTER TABLE `category` DISABLE KEYS */;
-/*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
 -- Table structure for table `customer`
 --
-
 DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 create table  users
 	(
 		`user_id` varchar(20) primary key,
@@ -65,6 +40,7 @@ create table  users
 		);
 
 DROP TABLE IF EXISTS `USERADDRESS`;
+
 create table useraddress
 	(
 		`user_id` varchar(20) primary key,
@@ -77,40 +53,27 @@ create table useraddress
 		`zip_code` varchar(10),
 		foreign key(user_id) references `users`(user_id) on delete cascade on update cascade
 	);
-/*!40101 SET character_set_client = @saved_cs_client */;
-
 
 
 --
 -- Table structure for table `payment`
 --
-
 DROP TABLE IF EXISTS `payment`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `payment` (
   `paymentid` int NOT NULL AUTO_INCREMENT,
   `paymenttype` varchar(45) NOT NULL,
    PRIMARY KEY (`paymentid`)
 );
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `payment`
---
 
 LOCK TABLES `payment` WRITE;
-/*!40000 ALTER TABLE `payment` DISABLE KEYS */;
-/*!40000 ALTER TABLE `payment` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
 -- Table structure for table `product`
 --
-
 DROP TABLE IF EXISTS `product`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `product` (
   `product_id` varchar(20) NOT NULL,
   `name` varchar(45) NOT NULL,
@@ -128,25 +91,17 @@ CREATE TABLE `product` (
   CONSTRAINT `category_id` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `ownerid` FOREIGN KEY (`ownerid`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `product`
---
 
 LOCK TABLES `product` WRITE;
-/*!40000 ALTER TABLE `product` DISABLE KEYS */;
-/*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
 
 --
 -- Table structure for table `order`
 --
-
 DROP TABLE IF EXISTS `order`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `order` (
   `order_id` varchar(20) NOT NULL,
   `productid` varchar(20) NOT NULL,
@@ -163,29 +118,26 @@ CREATE TABLE `order` (
   CONSTRAINT `paymentid` FOREIGN KEY (`paymentid`) REFERENCES `payment` (`paymentid`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `productid` FOREIGN KEY (`productid`) REFERENCES `product` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `order`
---
 
 LOCK TABLES `order` WRITE;
-/*!40000 ALTER TABLE `order` DISABLE KEYS */;
-/*!40000 ALTER TABLE `order` ENABLE KEYS */;
 UNLOCK TABLES;
 
-drop table if exists `reviews`;
-create table `reviews`(
-	`product_id` varchar(20),
-	`order_id` varchar(20),
-	`rating` int ,
-	`review_date` timestamp,
-	primary key(`product_id`,`order_id`),
-	constraint check_rating check (`rating` < 6 AND `rating` > 0),
-	foreign key (`order_id`) references `order`(`order_id`) on update cascade,
-	foreign key (`product_id`) references `product`(`product_id`) on update cascade
+--
+-- Table structure for table `reviews`
+--
+-- drop table if exists `reviews`;
 
-);
+-- create table `reviews`(
+-- 	`product_id` varchar(20),
+-- 	`order_id` varchar(20),
+-- 	`rating` int ,
+-- 	`review_date` timestamp,
+-- 	primary key(`product_id`,`order_id`),
+-- 	constraint check_rating check (`rating` < 6 AND `rating` > 0),
+-- 	foreign key (`order_id`) references `order`(`order_id`) on update cascade,
+-- 	foreign key (`product_id`) references `product`(`product_id`) on update cascade
+
+-- );
 
 
 INSERT into users(`user_id`,`user_name`,`pw`,`dob`,`fullname`,`phone_number`,`email_id`,`sex`,`deleted_flag`,`rating`) values('uaa001', 'Tejas_P' , 'tejurocks' , '01-03-1998' , 'Tejas Prashanth' , '9930291339' , 'tejuprash@gmail.com','M',false,1);
@@ -220,7 +172,6 @@ INSERT into useraddress(`user_id`,`flat_no`,`street_name`,`locality`,`city`,`sta
 INSERT into useraddress(`user_id`,`flat_no`,`street_name`,`locality`,`city`,`state`,`country`,`zip_code`) values('uaa014', '23' , 'HAL road' , 'banashankari' , 'Bangalore' , 'Karnataka' , 'India' ,'560085');
 INSERT into useraddress(`user_id`,`flat_no`,`street_name`,`locality`,`city`,`state`,`country`,`zip_code`) values('uaa015', '23' , 'lavelle road' , 'gandhinagar' , 'Bangalore' , 'Karnataka' , 'India' ,'560001');
 
-
 INSERT INTO category(`category_id`,`name`,`description`) VALUES('c001','vehicles','Cars,bikes,vehicle parts');
 INSERT INTO category(`category_id`,`name`,`description`) VALUES('c002','furniture','Household items');
 INSERT INTO category(`category_id`,`name`,`description`) VALUES('c003','electronics','TV,AC, fridge,laptops');
@@ -236,14 +187,3 @@ INSERT into product(`product_id`,`name`,`category_id`,`picture_url`,`description
 INSERT into product(`product_id`,`name`,`category_id`,`picture_url`,`description`,`cost`,`discount`,`features`,`ownerid`,`available_flag`) values('po006', 'shirts', 'c005', '/po006', 'Shirts from Levi', '100', '0','Size:M','uaa001',1);
 INSERT into product(`product_id`,`name`,`category_id`,`picture_url`,`description`,`cost`,`discount`,`features`,`ownerid`,`available_flag`) values('po007', 'HP laptop', 'c003', '/po007', 'HP x360 pavilion', '55000', '0','1TB HDD + 128GB SSD,8GB RAM,Warranty available;Battery in good condition','uaa014',1);
 INSERT into product(`product_id`,`name`,`category_id`,`picture_url`,`description`,`cost`,`discount`,`features`,`ownerid`,`available_flag`) values('po008', 'Asus Zenfone', 'c004', '/po008', 'Asus Zenfone 3', '20000', '0','6GB RAM; 64GB storage; 10MP rear camera','uaa010',1);
-
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-11-09 11:13:17
